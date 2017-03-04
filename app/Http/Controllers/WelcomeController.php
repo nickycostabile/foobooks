c52d97fd2a999e5ec88b20147bf13d4cd59ef97d
Nicky Costabile <nicolecostabile@gmail.com>
2017-03-04 12:32:12 -0500

    Examples from Lecture 6

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /** 
+    * Get
+    * /
+    */
+    public function __invoke() {
+		return view('welcome');
+    }
+}
