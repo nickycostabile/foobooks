5735c22e4e4e5ce9ac54e71125581d68fa1f2722
Nicky Costabile <nicolecostabile@gmail.com>
2017-03-11 12:50:13 -0500

    Install Laravel 5 Log Viewer Package

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ Route::any('/practice/{n?}', 'PracticeController@index');
 if(config('app.env') == 'local') {
 	Route::get('/logs', function() { });
 }
+
+/* Laravel 5 Log Viewer Package */
+if(config('app.env') == 'local') {
+	Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
